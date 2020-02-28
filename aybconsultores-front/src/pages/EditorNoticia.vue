61bcfa65afb393c59c294e4a79b68b2653479d3e
Juan Quedena <juan.quedena@bbva.com>
2020-02-27 23:57:25 -0500

    .

--- aybconsultores-front/src/pages/EditorNoticia.vue
+++ aybconsultores-front/src/pages/EditorNoticia.vue
@@ -1,40 +1,84 @@
 <template>
   <div class="container-fluid">
-    <h1>Noticias</h1>
+    <h3 class="pt-4 pb-3">Editor de noticias</h3>
     <div class="row">
       <div class="col-lg-6">
-        <ag-grid-vue
-          class="app-grid ag-theme-balham"
-          :columnDefs="columnDefs"
-          :rowData="rowData"
-          @grid-ready="onGridReady" />
+        <div class="row">
+          <div class="col-lg-2">
+            <label class="form-control">Foto</label>
+          </div>
+          <div class="col-lg-10">
+            <input type="text" class="form-control"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-2">
+            <label class="form-control">Titulo</label>
+          </div>
+          <div class="col-lg-10">
+            <input type="text" class="form-control"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-2">
+            <label class="form-control">Categor&iacute;a</label>
+          </div>
+          <div class="col-lg-10">
+            <select name="" id="" class="form-control">
+              <option value="">Finanzas</option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+            </select>
+          </div>
+        </div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row">
+          <div class="col-12">
+            <vue-editor v-model="postSelected.content" />
+          </div>
+        </div>
       </div>
-      <div class="col-lg-6">
-        <vue-editor v-model="content" />
+      <div class="col-lg-6 d-sm-none d-md-block">
+        <div class="row">
+          <div class="col-lg-12">
+            <h3>Vista previa</h3>
+          </div>
+        </div>
+        <div class="row app-visor-post">
+          <div class="col-lg-12">
+            <div class="row"><Post :post="postSelected" /></div>
+            <div class="row"><div v-html="postSelected.content"></div></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 import { VueEditor } from "vue2-editor";
-import { AgGridVue } from "ag-grid-vue";
 
 export default {
   name: 'Noticias',
-  components: { VueEditor, AgGridVue },
+  components: { VueEditor },
   props: {
     params: Object
   },
   data: () => ({
     content: "<h1>Some initial content</h1>",
-    columnDefs: [
-      {field: 'title', headerName: "Titulo"},
-      {field: 'category', headerName: "Categoría"},
-      {field: 'date', headerName: "Fecha"},
-      {field: 'likes', headerName: "# Me Gusta"},
-      {field: 'comments', headerName: "# Comentarios"}
-    ],
-    rowData: [ {"title": "dd", "category": "Finanzas", "date": "12/02/2020 12:12:12", "likes": 2, "comments": 5} ]
+    postSelected: {
+      "id": 1,
+      "photo": "https://cloud/img.png",
+      "content": "Content <h1>Title</h1>",
+      "title": "Noticia  1",
+      "category": "Finanzas",
+      "authorId": 1,
+      "created": "12/12/2020",
+      "updated": "12/12/2020",
+      "likes": 10,
+      "numberOfComments": 34
+    }
   }),
   methods: {
     onGridReady(params) {
